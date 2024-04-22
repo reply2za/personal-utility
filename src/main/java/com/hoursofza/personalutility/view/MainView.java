16e98db0a6633fad435f5c198cea5c71af7b9958
Zain <reply2zain@gmail.com>
2024-04-21 23:45:03 -0400

    add close window keybinding

--- src/main/java/com/hoursofza/personalutility/view/MainView.java
+++ src/main/java/com/hoursofza/personalutility/view/MainView.java
@@ -1,13 +1,15 @@
 package com.hoursofza.personalutility.view;
 
-import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.*;
 
 import org.springframework.stereotype.Component;
 
 import com.formdev.flatlaf.FlatLightLaf;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.beans.PropertyChangeListener;
 
 @Component
 public class MainView {
@@ -23,9 +25,11 @@ public class MainView {
     }
 
     MainView(WallpaperView wallpaperView) {
-
         mainFrame.setTitle("Personal Utility");
         JTabbedPane tabbedPane = new JTabbedPane();
+        int metaKey = Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx();
+        tabbedPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, metaKey), "doEnterAction");
+        tabbedPane.getActionMap().put("doEnterAction", new CloseAction());
         tabbedPane.addTab("single", wallpaperView.initSingleWallpaperSection());
         tabbedPane.addTab("interval", wallpaperView.initIntervalWallpaperSection());
         mainFrame.add(tabbedPane);
@@ -38,6 +42,43 @@ public class MainView {
             mainFrame.setVisible(true);
         });
         mainFrame.setVisible(true);
+    }
+
+    private final class CloseAction implements Action {
+
+        @Override
+        public Object getValue(String s) {
+            return null;
+        }
+
+        @Override
+        public void putValue(String s, Object o) {
+
+        }
+
+        @Override
+        public void setEnabled(boolean b) {
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+        @Override
+        public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+
+        }
+
+        @Override
+        public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            mainFrame.setVisible(false);
+        }
     }
 
 }
